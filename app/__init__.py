0495d7b3fc3d17db6379b290819278cd35f4a3bf
jgodoyc <jgodoyc@hotmail.com>
2024-11-25 16:07:26 -0300

    Documentacion API, validaciones API y refactorizacion

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 from .routes import api
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -9,5 +11,14 @@ def create_app():
     CORS(app)
 
     app.register_blueprint(api)
+    
+    SWAGGER_URL = '/swagger'
+    API_URL = '/openapi.yaml'
+    swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': "TalaTrivia API"})
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+    
+    @app.route('/openapi.yaml')
+    def serve_openapi():
+        return send_from_directory(os.path.abspath(os.path.dirname(__file__)), 'openapi.yaml')
 
     return app
